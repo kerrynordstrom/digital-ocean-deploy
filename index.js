08cd757c7ca381e41b15276b3894deab5c0bc128
Kerry Nordstrom <kerry.a.nordstrom@gmail.com>
2018-06-06 19:43:15 -0500

    Adding mongodb and assert packages and logic

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const app = express()
 
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+
+// Connection URL
+const url = 'mongodb://104.236.23.173:27017';
+
+// Database Name
+const dbName = 'admin';
+
 app.get('/', (req, res) => {
   res.send('Hey, I\'m a Node.js app!')
 })
@@ -8,3 +17,13 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
   console.log('Server is up on 3000')
 })
+
+// Use connect method to connect to the server
+MongoClient.connect(url, function (err, client) {
+  assert.equal(null, err);
+  console.log("Connected successfully to server");
+
+  const db = client.db(dbName);
+
+  client.close();
+});
